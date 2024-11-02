4520f41908048c07fa3c83396dfc4b7690ff9dd9
walter carrasco <carrascowalter440@gmail.com>
2024-11-02 01:48:15 -0300

    first commit

---
+++ includes/admin-menu.php
@@ -0,0 +1,19 @@
+<?php
+
+function book_uploader_register_post_type() {
+    register_post_type('libro', [
+        'labels' => [
+            'name' => __('Libros'),
+            'singular_name' => __('Libro')
+        ],
+        'public' => true,
+        'has_archive' => true,
+        'supports' => ['title', 'editor', 'thumbnail'],
+        'menu_icon' => 'dashicons-book',
+        'rewrite' => [
+            'slug' => 'libro',
+            'with_front' => false
+        ]
+    ]);
+}
+add_action('init', 'book_uploader_register_post_type');
